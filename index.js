58f44caf3e6d061d267e1dd3e078fabddc72156b
smadjapa <smadjapa@gmail.com>
2022-10-13 16:23:22 +0300

    Adding the htttps

--- index.js
+++ index.js
@@ -1,67 +1,86 @@
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var url = require('url');
 var config = require('./config');
-const { request } = require('https');
 var StringDecoder = require('string_decoder').StringDecoder;
 
-// Defining the server
-var server = http.createServer(function(req,res){
-    
-    //Geting the path
-    var parsedUrl = url.parse(req.url, true);
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    //Get the query strings
-    var queryObj = parsedUrl.query;
-
-    //Get the method
-    var method = req.method.toLowerCase();
-
-    //Get the headers
-    var headers = req.headers;
-
-    //Get body
-    var bufffer = '';
-    var decoder = new StringDecoder('utf-8');
-    
-    req.on('data', function(data){
-        bufffer += decoder.write(data);
-    });
-
-    req.on('end', function(){
-        bufffer += decoder.end();
-
-        var choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-        var data  = {
-            'trimmedPath' : trimmedPath,
-            'queryStringObject' : queryObj,
-            'method' : method,
-            'headers' : headers,
-            'payload' : bufffer
-        };
-
-        choosenHandler(data,function(statusCode, payload){
-
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-            payload = typeof(payload) == 'object' ? payload : {};
-
-            var payladString = JSON.stringify(payload);
-
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payladString);
-
-            console.log("We are returning this response", statusCode, payladString);
-        });
-    });
-
+// Instantiating the http server
+var httpserver = http.createServer(function(req,res){
+    unifiedServer(req, res);
 });
 
-//Start the server, and have it listen to port 3000
-server.listen(config.port, function(){
-    console.log("The server is running on port " + config.port + " in "+config.envName+" mode");
+//Start the http server
+httpserver.listen(config.httpPort, function(){
+    console.log("The server is running on port " + config.httpPort);
 });
+
+// Instantiating the https server
+var httpsServerOptions = {
+    'key' : fs.readFileSync('./https/key.pem'),
+    'cert' : fs.readFileSync('./https/cert.pem')
+};
+var httpsServer = https.createServer(httpsServerOptions,function(req,res){
+    unifiedServer(req, res);
+});
+
+//Start the https server
+httpsServer.listen(config.httpsPort, function(){
+    console.log("The server is running on port " + config.httpsPort);
+});
+
+//All the server logics for both the http and https
+var unifiedServer = function(req, res){
+     //Geting the path
+     var parsedUrl = url.parse(req.url, true);
+     var path = parsedUrl.pathname;
+     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+ 
+     //Get the query strings
+     var queryObj = parsedUrl.query;
+ 
+     //Get the method
+     var method = req.method.toLowerCase();
+ 
+     //Get the headers
+     var headers = req.headers;
+ 
+     //Get body
+     var bufffer = '';
+     var decoder = new StringDecoder('utf-8');
+     
+     req.on('data', function(data){
+         bufffer += decoder.write(data);
+     });
+ 
+     req.on('end', function(){
+         bufffer += decoder.end();
+ 
+         var choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+ 
+         var data  = {
+             'trimmedPath' : trimmedPath,
+             'queryStringObject' : queryObj,
+             'method' : method,
+             'headers' : headers,
+             'payload' : bufffer
+         };
+ 
+         choosenHandler(data,function(statusCode, payload){
+ 
+             statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+             payload = typeof(payload) == 'object' ? payload : {};
+ 
+             var payladString = JSON.stringify(payload);
+ 
+             res.setHeader('Content-Type', 'application/json');
+             res.writeHead(statusCode);
+             res.end(payladString);
+ 
+             console.log("We are returning this response", statusCode, payladString);
+         });
+     });
+}
 
 //Define the handlers
 var handlers = {};
